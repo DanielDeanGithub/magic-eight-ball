9ccb93d9e4c78f8b57cbdb8364984242a7a1a0b4
DanDean191 <github-new@inboxrefresh.com>
2024-02-13 18:47:13 +0000

    Added randomNumber variable to generate a value between 0 and 7

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ username ? console.log(`Hello ${userName}.`) : console.log('Hello.');
 const userQuestion = 'Will I win the Lottery this week?';
 
 console.log('Question: ' + userQuestion);
+
+const randomNumber = Math.floor(Math.random() * 8);
