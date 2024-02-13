0056225dcd2c491b7b36d70a9ba490f0abeb6f24
DanDean191 <github-new@inboxrefresh.com>
2024-02-13 18:42:17 +0000

    Output userQuestion to screen

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ let userName = '';
 username ? console.log(`Hello ${userName}.`) : console.log('Hello.');
 
 const userQuestion = 'Will I win the Lottery this week?';
+
+console.log('Question: ' + userQuestion);
